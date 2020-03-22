79b23dd98c27fb90714bb8cbc70978e3663f7b96
Adwait Arajaria <adeveloper121@gmail.com>
2020-03-22 10:44:21 +0530

    Player movement basics done.

---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public VariableJoystick joystick;
+    public float speed = 0.2f;
+    public float jumpForce = 100f;
+
+    Rigidbody rb;
+
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _x = joystick.Horizontal;
+        
+        if(rb.velocity.y <= 1.65f)
+        {
+            rb.AddForce(Vector3.down * Time.deltaTime * 1150f, ForceMode.Acceleration);
+        }
+
+        rb.MovePosition(rb.position + (Vector3.right * _x * speed));
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ 2D Platformer/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public VariableJoystick shootJoystick;
+    public LineRenderer aimLine;
+    public GameObject bullet;
+    public float shootVelocity;
+
+    Vector3 recDir;
+    bool canShoot;
+    bool isLoaded;
+
+    private void Update()
+    {
+        if(isLoaded == true)
+        {
+            canShoot = true;
+        }
+
+        Vector3 dir = new Vector3(shootJoystick.Horizontal, shootJoystick.Vertical, 0);
+
+        if(dir.magnitude >= 0.5f)
+        {
+            RenderAim(dir);
+        }
+        else
+        {
+            RenderAim(Vector3.zero);
+        }
+
+        if (dir != Vector3.zero)
+        {
+            canShoot = false;
+            isLoaded = true;
+            recDir = dir;
+        }
+
+        if(dir == Vector3.zero && canShoot == true && recDir.magnitude >= 0.5f)
+        {
+            GameObject bulletGO = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+
+            bulletGO.GetComponent<Bullet>().GetDirection(recDir, shootVelocity);
+
+            isLoaded = false;
+            canShoot = false;
+        }
+    }
+
+    void RenderAim(Vector3 direction)
+    {
+        aimLine.SetPosition(0, transform.position);
+
+        Vector3 _direction = transform.position + (direction.normalized * 8f);
+
+        aimLine.SetPosition(1, _direction);
+    }
+}
